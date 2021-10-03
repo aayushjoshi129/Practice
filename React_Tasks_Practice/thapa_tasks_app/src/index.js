3bd93b9a704c74e3885e9bfc5dab674f08cb5348
aayushjoshi129 <aayushjoshi129@gmail.com>
2021-10-03 21:43:59 +0530

    Third Task Completed Thapa_Technical

--- React_Tasks_Practice/thapa_tasks_app/src/index.js
+++ React_Tasks_Practice/thapa_tasks_app/src/index.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const name = "Aayush Joshi"
-const CurrDate = new Date().toLocaleDateString();
-const CurrTime = new Date().toLocaleTimeString();
+
+const styleGreeting = {
+  color:""
+}
+
+const time = new Date(2021,10,3,22).getHours();
+let Greeting = ""
+if (time>=0 && time<12){
+  Greeting = "Good Morning"
+  styleGreeting.color = "Green"
+}
+else if (time>=12 && time<18){
+  Greeting = "Good Afternoon"
+  styleGreeting.color = "Orange"
+}
+else if (time>=18 && time<21){
+  Greeting = "Good Evening"
+  styleGreeting.color = "Blue"
+}
+else
+{
+  Greeting = "Good Night"
+  styleGreeting.color = "Red"
+}
+
 
 ReactDOM.render(
-<React.Fragment>  
-  <h1>My Name is {name}</h1>
-  <p>Current Date is {CurrDate}</p>
-  <p>Current Time is {CurrTime}</p>
-</React.Fragment>,
-document.getElementById("root")
+  <h1>Hello Sir , <span style={styleGreeting}>{Greeting}</span> </h1>,
+  document.getElementById("root")
 )
