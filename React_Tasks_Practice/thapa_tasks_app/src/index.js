4ec51179a4802155004ef25d46cb3ae5b1e7890f
aayushjoshi129 <aayushjoshi129@gmail.com>
2021-10-03 22:03:06 +0530

    Fourth Task Completed Thapa_Technical(Render Using Components) and Added CSS to existing code

--- React_Tasks_Practice/thapa_tasks_app/src/index.js
+++ React_Tasks_Practice/thapa_tasks_app/src/index.js
@@ -1,33 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from "./App"
+import "./index.css"
 
-
-const styleGreeting = {
-  color:""
-}
-
-const time = new Date(2021,10,3,22).getHours();
-let Greeting = ""
-if (time>=0 && time<12){
-  Greeting = "Good Morning"
-  styleGreeting.color = "Green"
-}
-else if (time>=12 && time<18){
-  Greeting = "Good Afternoon"
-  styleGreeting.color = "Orange"
-}
-else if (time>=18 && time<21){
-  Greeting = "Good Evening"
-  styleGreeting.color = "Blue"
-}
-else
-{
-  Greeting = "Good Night"
-  styleGreeting.color = "Red"
-}
-
-
-ReactDOM.render(
-  <h1>Hello Sir , <span style={styleGreeting}>{Greeting}</span> </h1>,
-  document.getElementById("root")
-)
+ReactDOM.render( <App/>, document.getElementById("root")) // Used App Component
