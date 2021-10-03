960b62b07deb9559d94a35affe6fb621868ee983
aayushjoshi129 <aayushjoshi129@gmail.com>
2021-10-03 17:44:05 +0530

    first task completed Thapa_Technical

---
+++ React_Tasks_Practice/thapa_tasks_app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <>
+  <h1>Favourite Lists</h1>
+  <p>My Favourite Films of Ammy Virk</p>
+  <ol>
+    <li>Qismat</li>
+    <li>Qismat 2</li>
+    <li>Sufna</li>
+    <li>Puadaa</li>
+    <li>Laung Laachi</li>
+  </ol>
+  </>,document.getElementById("root")
+)
