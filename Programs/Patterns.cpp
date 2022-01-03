13b74d925077ec362ae3315aa78cf02f5385ad25
aayushjoshi129 <aayushjoshi129@gmail.com>
2022-01-03 22:03:17 +0530

    Completed Lecture 4 Code Help CPP

--- Programs/Patterns.cpp
+++ Programs/Patterns.cpp
@@ -43,6 +43,456 @@ int main(){
     
   // Pattern 4
 
+    // while(i<=n){
+    //   int j=1;
+    //   while(j<=n){
+    //     cout<<n-j+1<<" ";
+    //     j+=1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 5
+
+    // int count=1;
+    // while(i<=n){
+    //   int j=1;
+    //   while(j<=n){
+    //     cout<<count<<" ";
+    //     count=count+1;
+    //     j+=1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 6
+
+    // while(i<=n){
+    //   int j =1;
+    //   while(j<=i){
+    //     cout<<"*"<<" ";
+    //     j+=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 7
+
+    // while(i<=n){
+    //   int j=1;
+    //   while(j<=i){
+    //     cout<<i<<" ";
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 8
+
+    // int count=1;
+    // while(i<=n){
+    //   int j=1;
+    //   while(j<=i){
+    //     cout<<count<<" ";
+    //     count = count + 1;
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 9
+
+    // while(i<=n){
+    //   int j=1;
+    //   while(j<=i){
+    //     cout<<i+j-1<<" ";
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Alternate Way of Pattern 9
+
+    //   while(i<=n){
+    //   int j=1,count=i;
+    //   while(j<=i){
+    //     cout<<count<<" ";
+    //     count=count+1;
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 10
+
+    // while(i<=n){
+    //   int j=1;
+    //   while(j<=i){
+    //     cout<<i-j+1<<" ";
+    //       j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 11
+
+    // while(i<=n){
+    //   int j=1;
+    //   while(j<=n){
+    //     char ch = 'A'+i-1;
+    //     cout<<ch<<" ";
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 12
+
+    //   while(i<=n){
+    //   int j=1;
+    //   while(j<=n){
+    //     char ch = 'A'+j-1;
+    //     cout<<ch<<" ";
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 13
+
+    //     int count = 1;
+    //     while(i<=n){
+    //     int j=1;
+    //     while(j<=n){
+    //       char ch = 'A'+count-1;
+    //       cout<<ch<<" ";
+    //       count+=1;
+    //       j=j+1;
+    //     }
+    //     cout<<endl;
+    //     i=i+1;
+    //   }
+
+  // Pattern 14
+
+    //   while(i<=n){
+    //   int j=1;
+    //   while(j<=n){
+    //     char ch = 'A'+i+j-2;
+    //     cout<<ch<<" ";
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Alternate Way of Pattern 14
+
+    //   while(i<=n){
+    //     int j=1;
+    //     char ch = 'A'+i-1;
+    //   while(j<=n){
+    //     cout<<ch<<" ";
+    //     ch=ch+1;
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 15
+
+    //   while(i<=n){
+    //   int j=1;
+    //   while(j<=i){
+    //     char ch = 'A'+i-1;
+    //     cout<<ch<<" ";
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 16
+
+    // int count = 1;
+    // while(i<=n){
+    //   int j=1;
+    //   while(j<=i){
+    //     char ch = 'A'+count-1;
+    //     cout<<ch<<" ";
+    //     count+=1;
+    //     j+=1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 17
+
+    // while(i<=n){
+    //   int j=1;
+    //   while(j<=i){
+    //     char ch = 'A'+i+j-2;
+    //     cout<<ch<<" ";
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 18
+
+    //   while(i<=n){
+    //   char ch = 'A'+n-i;
+    //   int j=1;
+    //   while(j<=i){
+    //     cout<<ch<<" ";
+    //     ch+=1;
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 19
+
+    // while(i<=n){
+    //   int space = n-i;
+    //   while(space){
+    //     cout<<" ";
+    //     space-=1;
+    //   }
+    //   int j=1;
+    //   while(j<=i){
+    //   cout<<"*";
+    //   j+=1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
+
+  // Pattern 20
+
+    // while(i<=n){
+    //   int j=i;
+    //   while(n-j+1){
+    //     cout<<"*"<<" ";
+    //     j+=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 21
+
+    // while(i<=n){
+    //   int space = i-1;
+
+    //   // print spaces 
+    //   while(space){
+    //     cout<<" ";
+    //     space-=1;
+    //   }
+
+    //   // print stars
+    //   int stars=n-i+1;
+    //   while(stars){
+    //     cout<<"*";
+    //     stars-=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 22
+
+    //   while(i<=n){
+    //   int space = i-1;
+
+    //   // print spaces 
+    //   while(space){
+    //     cout<<" ";
+    //     space-=1;
+    //   }
+
+    //   // print stars
+    //   int stars=n-i+1;
+    //   while(stars){
+    //     cout<<i;
+    //     stars-=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 23
   
+    //   while(i<=n){
+    //   int space = n-i;
+
+    //   // print spaces 
+    //   while(space){
+    //     cout<<" ";
+    //     space-=1;
+    //   }
+
+    //   // print stars
+    //   int j=1;
+    //   while(j<=i){
+    //     cout<<i;
+    //       j+=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 24
+
+    // while(i<=n){
+    //   int j=n-i+1;
+    //   while(j){
+    //     cout<<j<<" ";
+    //     j-=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 25
+
+    //   while(i<=n){
+    //   int j=1;
+    //   while(j<=n-i+1){
+    //     cout<<j<<" ";
+    //     j+=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 26
+
+    // while(i<=n){
+
+    //   // print spaces
+    //   int spaces = i-1;
+    //   while(spaces){
+    //     cout<<" ";
+    //     spaces-=1;
+    //   }
+
+    //   // print numbers
+    //   int j=i;
+    //   while(j<=n){
+    //     cout<<j;
+    //     j+=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 27
+    
+    //   int count=1;
+    //   while(i<=n){
+    //   int space = n-i;
+
+    //   // print spaces 
+    //   while(space){
+    //     cout<<" ";
+    //     space-=1;
+    //   }
+
+    //   // print stars
+    //   int j=1;
+    //   while(j<=i){
+    //     cout<<count;
+    //     count+=1;
+    //       j+=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+    // }
+
+  // Pattern 28
+
+    // while(i<=n){
+
+    //   // print spaces
+    //   int space = n-i;
+    //   while(space){
+    //     cout<<" ";
+    //     space-=1;
+    //   }
+
+    //   // print first(left) triangle
+    //   int j = 1;
+    //   while(j<=i){
+    //     cout<<j;
+    //     j+=1;
+    //   }
+
+    //   // print second(right) triangle
+    //   int start = i-1;
+    //   while(start){
+    //     cout<<start;
+    //     start-=1;
+    //   }
+    //   cout<<endl;
+    //   i+=1;
+
+    // }
+
+  // Pattern 29
+
+    // while(i<=n){
+    //   int j = 1;
+
+    // // first(left) triangle
+    //   while(j<=n-i+1){
+    //     cout<<j<<" ";
+    //     j+=1;
+    //   }
+
+
+    // // second(left stars) triangle 
+    //       int k = i-1;
+    //     while(k){
+    //       cout<<"* * ";
+    //       k-=1;
+    //     }
+
+    // // third right stars can be discarded when giving 2 stars in previous one
+
+    // // // third(right stars) triangle 
+    // //       int x = i-1;
+    // //     while(x){
+    // //       cout<<"* ";
+    // //       x-=1;
+    // //     }
+
+
+    // // fourth(right) triangle
+    // int y=n-i+1;
+    // while(y){
+    //   cout<<" "<<y;
+    //   y-=1;
+    // }
+
+
+    //   cout<<endl;
+    //   i=i+1;
+    // }
 
 }
--- Programs/tempCodeRunnerFile.cpp
+++ Programs/tempCodeRunnerFile.cpp
@@ -1,1 +1,10 @@
-cout<<n<<" is a Prime Number"<<endl;
+    //   while(i<=n){
+    //   int j=1;
+    //   while(j<=n){
+    //     char ch = 'A'+i+j-2;
+    //     cout<<ch<<" ";
+    //     j=j+1;
+    //   }
+    //   cout<<endl;
+    //   i=i+1;
+    // }
