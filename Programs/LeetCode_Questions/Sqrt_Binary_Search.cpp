7c272908d57a5a10bdada91e8a73f9f48b831da1
aayushjoshi129 <aayushjoshi129@gmail.com>
2022-01-20 21:09:18 +0530

    Completed Lecture 14 Code Help(Sqrt Using Precison using Binary Search)

--- Programs/LeetCode_Questions/Sqrt_Binary_Search.cpp
+++ Programs/LeetCode_Questions/Sqrt_Binary_Search.cpp
@@ -3,15 +3,15 @@
 #include <iostream>
 using namespace std;
 
-long long int BinarySearch(long long int key)
+int BinarySearch(int key)
 {
-  long long int start = 0;
-  long long int end = key;
-  long long int mid = (start + end) / 2; // Mid can also be calculated as mid = (start + (end-start))/2    --> It will prevent from overflow errors
-  long long int ans = -1;
+  int start = 0;
+  int end = key;
+  int mid = (start + end) / 2; // Mid can also be calculated as mid = (start + (end-start))/2    --> It will prevent from overflow errors
+  int ans = -1;
   while (start <= end)
   {
-    long long int square = mid*mid;
+    int square = mid * mid;
     // Matches Value
     if (key == square)
     {
@@ -34,15 +34,31 @@ long long int BinarySearch(long long int key)
   return mid;
 }
 
+double precision(int n, int precision, int tempSol)
+{
+  double ans=tempSol;
+  double factor = 1;
+  for (int i = 0; i < precision; i++)
+  {
+    factor /= 10;
+    for (double j = ans; j * j < n; j = j + factor)
+    {
+      ans = j;
+    }
+  }
+  return ans;
+}
+
 int main()
 {
-  long long int key;
+  int key;
   cout << "Enter Key to find sqrt : ";
   cin >> key;
 
-  long long int num = BinarySearch(key);
+  int num = BinarySearch(key);
 
-  cout << num << endl;
+  cout<<precision(key,3,num);
+  // cout << num << endl;
 
   return 0;
 }
